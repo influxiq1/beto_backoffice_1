7c5dd0dd87945ba57be90da58f9b3e26c3152955
mahitosh-dey <mahitosh.influxiq@gmail.com>
2020-05-18 10:16:21 +0530

    leads up-to-date

--- src/app/manageleads/manageleads.component.ts
+++ src/app/manageleads/manageleads.component.ts
@@ -82,7 +82,6 @@ hidedeletebutton:false, // (hide delete button)
 hideviewbutton:true, // (hide view button)
 hidestatustogglebutton:true, // (hide status toggle button)
 hideaction:false, // (hide action column)
-basecondition:{created_by:this.cookieservice.get('userid')},
 
 tableheaders:['fullname','company','website','email','phoneno','mobile','address','only_productname','emailStatus'], //not required (table header name)
 custombuttons:[
